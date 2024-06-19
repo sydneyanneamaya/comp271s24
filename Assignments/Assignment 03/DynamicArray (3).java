bf30d2f5e6c6dbde9a7b2ec4b82f068fdc4bea4e
sydneyanneamaya <samaya@luc.edu>
2024-06-19 01:51:18 +0000

    edits to dynamicArray

--- Assignments/Assignment 03/DynamicArray (3).java
+++ Assignments/Assignment 03/DynamicArray (3).java
@@ -98,5 +98,34 @@ public class DynamicArray implements BasicTools {
         return canBeAdded;
     } // method addUnique
 
-    
+    /**
+     * Determines if this DynamicArray intersects the specified DynamicArray (other).
+     *
+     * @param other DynamicArray to determine if it intersects with this object
+     * @return true if two DynamicArrays have a common entry; false otherwise.
+     *
+     * To see if there is a common entry, each element in the arrays must be compared
+     * to one another to see if they are equal
+     *
+     * Use a while loop to traverse the object array and end the loop
+     * when a match is found or when the entire array is traversed
+     *    while ( some boolean is false && i<this.position) {
+     *       do something that returns boolean as true or false;
+     *       i++;
+     *     }
+     *  
+     * Use method contains to see if other[i] is in this object
+     *     this.contains(other[i])
+     * if contains returns true then intersects returns true
+     */
+    public boolean intersects(DynamicArray other){
+        boolean match = false;
+        int i = 0;
+        while (match == false && i < this.position){
+            match = this.contains(other.data[i]);
+            i++;
+        }
+        return match;
+    }
+
 } // class DynamicArray
