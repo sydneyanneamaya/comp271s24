b7c7badd8b5b108e0ffab9157c75526ffa623364
sydneyanneamaya <samaya@luc.edu>
2024-08-05 21:46:34 -0400

    Update Sorting271.java

--- Assignments/Assignment 10/Sorting271.java
+++ Assignments/Assignment 10/Sorting271.java
@@ -5,51 +5,73 @@ public class Sorting271 {
  * 
  */
 
- public int[] merge(int[] left, int[] right){
+ public static int[] merge(int[] left, int[] right){
     /**
      * I created and initialized all my variables 
      */
     //tracks the index when right is added to merged array
-    int count = 0;
+    int count = left.length;
     //stores the value of merge[i]
     int current = 0;
-    //stores the value of merge[j]
-    int other = 0;
     //create an array that is the length of both parameters combined
     int [] merged = new int[left.length + right.length];
     //check to see if arrays are null
     if (left != null && right != null){
       //traverse through left and add the values to merged
-      for(int i = 0; i <= left.length; i++){
+      for(int i = 0; i <= left.length -1; i++){
           merged[i] = left[i];
       }
       //traverses through right and add the valued to merged starting at the
       //index where left stopped
-      for(int i = left.length + 1; i <= right.length; i++){
-        merged[i] = right[count];
+      for(int i = 0; i <= right.length -1; i++){
+        merged[count] = right[i];
         count++;
       } 
       //traverses through merge and compare each value at merge[i] to every 
       //other value in the array
-      for(int i = 0; i <= merged.length;i++){
+      for(int i = 0; i <= merged.length -1;i++){
         //store the int at merge[i]
-        current = merged[i];
         /**
-         * traverses through merge and compares current to every other value 
-         * (which is stoed in other) and if current is greater than merge
+         * traverses through merge and compares i to every other value 
+         * and if the value at index[i] is greater than index[j] 
          * the two ints swap locations in the array since the greater value should 
          * be further down the array
          */
-        for(int j = 0; j <= merged.length; j++){
-            other = merged[j];
-            if( current > merged[i]){
-                merged[i] = other;
+        //start at i + 1 so the loop do not start at the same location
+        //also starting at i + 1 assumes the indexes before i are already sorted 
+        for(int j = i + 1; j < merged.length; j++){
+          //store value of current i 
+          current = merged[i];
+          //if the int at the index before i is less than current than the two ints
+          //swap locations in the array
+            if( merged[j] < merged[i]){
+                merged[i] = merged[j];
                 merged[j] = current;
             }
           } 
-       }
-    }
+        }
+      }
     return merged;
+  }
+
+
+/**
+ * Write a method int[] sort(int[] array) that takes an integer array with 2n
+ * elements and sorts it in ascending order, by using the technique described above. 
+ */
+public int[] sort(int[] array){
+  
+  return array;
 }
 
+public static void main(String [] args){
+  int[] a = {1,2,8,9};
+  int [] b = {0,5,6,7};
+  int [] c = merge(a,b);
+  for (int i = 0; i <= c.length -1;i++){
+    System.out.println(c[i] + ",");
+  }
+ }
+
 }//class Sorting271
+
